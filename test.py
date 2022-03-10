a70992f5abeb22d8c8f805b0598b31e47e53dd68
Ilaria <nicolamigone179@gmail.com>
2022-03-10 04:36:47 +0000

    Added primary version of unwrapJS, refractoring

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from unwrap import unwrapCSS as u
-
+from unwrap import unwrapJS as j
+from unwrap import wrap
 if __name__ == "__main__":
-    u("test/test0.css", result="test/test0.txt")
-    u("test/test1.css", result="test/test1.txt")
-    u("test/test2.css", result="test/test2.txt")
-    u("test/test3.css", result="test/test3.txt")
+    # u("test/test0.css", result="test/test0.txt")
+    # u("test/test1.css", result="test/test1.txt")
+    # u("test/test2.css", result="test/test2.txt")
+    # u("test/test3.css", result="test/test3.txt")
+    j(wrap("test/test0.js"), result="test/test0.txt", text=True)
--- unwrap.py
+++ unwrap.py
@@ -1,3 +1,7 @@
+import re #for regex
+def wrap(file):
+    return ''.join([item.strip() for item in open(file, 'r').readlines()])
+
 def unwrapCSS(file, result="result.txt", indentsize=4):
     with open(file, 'r') as infile:
         data = infile.readlines()[0]
@@ -5,8 +9,7 @@ def unwrapCSS(file, result="result.txt", indentsize=4):
     for char in data:
         if char not in [';', '{', '}']:
             res += char
-            continue
-        if char == '{' or char == ';':
+        elif char in ['{', ';']:
             res += char
             res += '\n'
             res += ' '*indentsize
@@ -16,5 +19,48 @@ def unwrapCSS(file, result="result.txt", indentsize=4):
             res += '\n'
     if res[-1] == '\n':
         res = res[:-1]
+    with open(result, 'w') as infile:
+        infile.write(res)
+
+def unwrapJS(file, result="result.txt", indentsize=4, text=False):
+    """
+    First read the data
+    next identify tokens that require a newline and those that require more indent
+    second separate it between the tokens
+    add it all together
+    write into result
+    """
+    #1
+    if text:
+        data = file
+    else:
+        with open(file, 'r') as infile:
+            data = infile.read()
+    #2
+    newlines = [';']
+    moreindent = ['{']
+    lessindent = ['}']
+    indent = 0
+    res = ''
+    last = ''
+    for char in data:
+        if char in newlines:
+            res += char
+            res += '\n'
+            res += ' '*indent
+        elif char in moreindent:
+            res += char
+            res += '\n'
+            indent += indentsize
+            res += ' '*indent
+        elif char in lessindent:
+            if last == ';':
+                res = res[:-indentsize]
+            indent -= indentsize
+            res += ' '*indent
+            res += char
+        else:
+            res += char
+        last = char
     with open(result, 'w') as infile:
         infile.write(res)
